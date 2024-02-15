1862efe410b636eccc1600e56242f977d96cf424
metanmai <103983719+metanmai@users.noreply.github.com>
2024-02-15 18:34:05 +0530

    Made changes on vscode.

--- test.cpp
+++ test.cpp
@@ -1,21 +1,21 @@
-#include "../solution-template.hpp"
+#include "solution-template.hpp"
 
-void solve()
-{
-	int i, j, n, m;
-	vi
+void solve() {
+    int i, j, k, n, m;
+    cin >> n;
+
+    fo(i, n) {
+        cout << i << " ";
+    }
 }
 
-int main()
-{
-	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-	srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+int main() {
+    int t = 1;
+    cin >> t;
 
-	int t = 1;
-//    cin >> t;
-	while(t--) {
-		solve();
-	}
+    while (t--) {
+        solve();
+    }
 
-	return 0;
+    return 0;
 }
